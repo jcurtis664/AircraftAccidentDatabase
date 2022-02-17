61d06c975f956c1bef6abc471075c75e5c617f83
Jared Herron <jlherron2000@gmail.com>
2022-02-17 18:00:21 -0500

    idk

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-//native modules
-const fs = require('fs');
-//npm modules
-const express = require('express');
-
-async function start() {
-    let app = express();
-    let server = app.listen(3000);
-    app.use(express.static('./public'));
-}
-
-start();
-=======
 const express = require('express')
 const prompt = require('prompt-sync')();
 const {MongoClient, AutoEncryptionLoggerLevel} = require('mongodb');
@@ -99,4 +85,3 @@ async function listDatabases(client)
 }
 
 main().catch(console.error);
->>>>>>> b2cc3070b15e9b962a92aec5c55e62e433ccfe63
