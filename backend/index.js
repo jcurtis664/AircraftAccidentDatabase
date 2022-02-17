b2cc3070b15e9b962a92aec5c55e62e433ccfe63
jcurtis664 <71359540+jcurtis664@users.noreply.github.com>
2022-02-17 17:57:24 -0500

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,87 @@
-const { MongoClient } = require('mongodb');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+const express = require('express')
+const prompt = require('prompt-sync')();
+const {MongoClient, AutoEncryptionLoggerLevel} = require('mongodb');
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
+var app = express()
+var server = app.listen(3000)
 
-// Database Name
-const dbName = 'myProject';
+app.use(express.static('./public'))
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
+console.log('running')
 
-  // the following code examples can be pasted here...
+const accident_schema = new mongoose.Schema({
+    tags: [String],
+    date: {
+        year: String,
+        month: String,
+        day: String
+    },
+    location: String,
+    aircraft: String,
+    airline: String,
+    fatalities: String,
+    ICAO_categories: [String],
+    AI_link: String,
+    AVSN_link: String,
+    synopsis: String,
+    languange_references: [{page_number: String, reference: String}]
+})
 
-  return 'done.';
+async function main()
+{
+    const uri = "mongodb+srv://admin:admin@aircraft-accidents.gmv7k.mongodb.net/aircraft_accidents_db?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+
+    try 
+    {
+        await client.connect();
+
+        await mongoose.connect(uri)
+
+        await listDatabases(client);
+
+        //await addNewAccident(client, createNewAccident());
+
+        const test = mongoose.model("test", accident_schema, "aircraft_accidents");
+
+        const accident_test = new test({
+            tags: ["2012", "pie", "dog"],
+            date: {year: "2012", month: "april", day: "28"},
+            location: "here",
+            aircraft: "grown man goose",
+            airline: "illegal",
+            fatalities: "36",
+            ICAO_categories: ["Anex 10"],
+            AI_link: "insert link here",
+            AVSN_link: "insert like here",
+            synopsis: "x = y",
+            languange_references: [{page_number: "103", reference: "germanic"}]
+        })
+
+        accident_test.save(function (err, book) {
+            if (err) return console.error(err);
+            console.log(book.name + " saved.");
+          });
+    }
+    catch (e)
+    {
+        console.error(e);
+    }
+
+    finally
+    {
+        await client.close();
+    }
 }
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+async function listDatabases(client)
+{
+    databaseList = await client.db().admin().listDatabases();
+
+    console.log("databases: ");
+    databaseList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+
+main().catch(console.error);
