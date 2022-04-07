796fedbf39cdd849c6544ed2f07c8195f4fdef4a
Jared Herron <jlherron2000@gmail.com>
2022-04-07 19:06:27 -0400

    searching

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const {MongoClient, AutoEncryptionLoggerLevel} = require('mongodb');
+const { MongoClient, AutoEncryptionLoggerLevel } = require('mongodb');
 const mongoose = require('mongoose');
 const http = require('http');
 const fs = require('fs');
@@ -22,16 +22,14 @@ const accident_schema = new mongoose.Schema({
     AI_link: String,
     AVSN_link: String,
     synopsis: String,
-    languange_references: [{page_number: String, reference: String}]
+    languange_references: [{ page_number: String, reference: String }]
 })
 
-async function connectToDatabase()
-{
+async function connectToDatabase() {
     const uri = "mongodb+srv://admin:admin@aircraft-accidents.gmv7k.mongodb.net/aircraft_accidents_db?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
 
-    try 
-    {
+    try {
         await client.connect();
         await mongoose.connect(uri);
 
@@ -39,19 +37,16 @@ async function connectToDatabase()
 
         await listDatabases(client);
     }
-    catch (e)
-    {
+    catch (e) {
         console.error(e);
     }
 
-    finally
-    {
+    finally {
         await client.close();
     }
 }
 
-async function listDatabases(client)
-{
+async function listDatabases(client) {
     databaseList = await client.db().admin().listDatabases();
 
     console.log("databases: ");
@@ -65,57 +60,94 @@ let server = http.createServer(async (req, res) => {
     if (req.url == '/') {
         res.write(fs.readFileSync('./frontend/index.html'));
     }
-    else if (req.url == '/style.css'){
+    else if (req.url == '/style.css') {
         res.write(fs.readFileSync('./frontend/style.css'));
     }
-    else if (req.url == '/tabs.js'){
+    else if (req.url == '/tabs.js') {
         res.write(fs.readFileSync('./frontend/tabs.js'));
     }
-    else if (req.url == '/reference.js'){
+    else if (req.url == '/reference.js') {
         res.write(fs.readFileSync('./frontend/reference.js'));
     }
-    else if (req.url == '/submitButton.js'){
+    else if (req.url == '/submitButton.js') {
         res.write(fs.readFileSync('./frontend/submitButton.js'));
     }
-    
-    else if (req.url == '/makeAccident') {
-        const buffers = [];
-
-        for await (const chunk of req) {
-          buffers.push(chunk);
-        }
-      
-        let buffer = Buffer.concat(buffers).toString()
-        const data = await JSON.parse(buffer);
-
-        let model = data.ans;
-        let file = new Int8Array(Object.values(data.file));
-    
-        let link = './accidentReports/'+model.accident_location+'-'+model.accident_date.year+'-'+model.accident_date.month+'-'+model.accident_date.day+'.pdf';
-        fs.writeFileSync(link, file)
-
-        const new_accident = new accident_model({
-            tags: model.accident_tags,
-            date: model.accident_date,
-            location: model.accident_location,
-            aircraft: model.accident_aircraft,
-            airline: model.accident_airline,
-            fatalities: model.accident_fatalities,
-            ICAO_categories: model.accident_icao_categories,
-            AI_link: link,
-            AVSN_link: model.accident_avsn_link,
-            synopsis: model.accident_synopsis,
-            languange_references: model.references
-        });
-    
-        //console.log(new_accident);
-    
-        //new_accident.save(function (err, accident) {
-        //    if (err) return console.error(err);
-        //    console.log(accident.name + " saved.");
-        //  });
+    else if (req.url == '/template.html') {
+        res.write(fs.readFileSync('./frontend/template.html'));
     }
-    res.end();
+    else if (req.url == '/defaultList.js') {
+        res.write(fs.readFileSync('./frontend/defaultList.js'));
+    }
+    else if (req.url == '/top20') {
+        let accident = await accident_model.find({});
+
+        accident.sort((ele1, ele2) => {
+            let date1 = Date.parse(ele1.date.year + ' ' + ele1.date.month + ' ' + ele1.date.day);
+            let date2 = Date.parse(ele2.date.year + ' ' + ele2.date.month + ' ' + ele2.date.day);
+            return date1 > date2
+        });
+
+        let top20 = [];
+        for (let i = 0; i < 20; i++) {
+            top20.push(accident[i]);
+        }
+
+        res.write(JSON.stringify(top20));
+    }
+
+    else if (req.url == '/makeAccident') {
+        try
+        {
+            const buffers = [];
+
+            for await (const chunk of req) {
+              buffers.push(chunk);
+            }
+          
+            let buffer = Buffer.concat(buffers).toString()
+            const data = await JSON.parse(buffer);
+    
+            let model = data.ans;
+            let file = new Int8Array(Object.values(data.file));
+        
+            let link = './accidentReports/'+model.accident_location+'-'+model.accident_date.year+'-'+model.accident_date.month+'-'+model.accident_date.day+'.pdf';
+            fs.writeFileSync(link, file)
+    
+            const new_accident = new accident_model({
+                tags: model.accident_tags,
+                date: model.accident_date,
+                location: model.accident_location,
+                aircraft: model.accident_aircraft,
+                airline: model.accident_airline,
+                fatalities: model.accident_fatalities,
+                ICAO_categories: model.accident_icao_categories,
+                AI_link: link,
+                AVSN_link: model.accident_avsn_link,
+                synopsis: model.accident_synopsis,
+                languange_references: model.references
+            });
+        
+            console.log(new_accident);
+        
+            new_accident.save(function (err, accident) {
+                if (err) 
+                {
+                    return console.error(err);
+                }
+                console.log(accident._id + " saved.");
+                res.write(JSON.stringify({msg: "success"}));
+                res.end();
+            });
+        }
+        catch (err)
+        {
+            res.write("failed");
+            res.end();
+        }
+    }
+    if (req.url != '/makeAccident') {
+        res.end();
+    }
 });
 
 server.listen(3000);
--- frontend/submitButton.js
+++ frontend/submitButton.js
@@ -1,56 +1,103 @@
+if (window.sessionStorage.displayMessage === 'true') {
+
+    var toast = document.getElementById("mtoast");
+    toast.innerHTML = "SUCCESS";
+    toast.classList.add("show");
+    toast.style.background = '#18e85d';
+    toast.style.display = 'inline';
+
+    window.sessionStorage.displayMessage = false;
+    setTimeout(function makeMessageDisappear() {
+        toast.style.display = 'none';
+    }, 5000);
+}
+
 async function submitAccident() {
-    let ans = {};
-    let date_input = new Date(document.getElementById('date').value);
+    try {
+        let ans = {};
 
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        //SUBMITTING THE WRONG DAY
 
-    ans.accident_date = {
-        year: date_input.getFullYear(),
-        month: date_input.getMonth() + 1,
-        day: date_input.getDay()
+        let date_input = new Date(document.getElementById('date').value);
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+        ans.accident_date = {
+            year: date_input.getFullYear(),
+            month: date_input.getMonth() + 1,
+            day: date_input.getDay()
+        }
+
+        ans.accident_location = document.getElementById('location').value;
+        ans.accident_aircraft = document.getElementById('aircraft').value;
+        ans.accident_airline = document.getElementById('airline').value;
+        ans.accident_fatalities = document.getElementById('fatalities').value;
+        ans.accident_synopsis = document.getElementById('synopsis').value;
+        ans.accident_avsn_link = document.getElementById('avsn_link').value;
+
+        ans.accident_icao_categories = document.getElementById('icao').value.split(",").map(function (item) {
+            return item.trim();
+        });
+
+        ans.accident_tags = document.getElementById('tags').value.split(",").map(function (item) {
+            return item.trim();
+        });
+        ans.accident_tags.push(months[parseInt(ans.accident_date.month) - 1]);
+        ans.accident_tags.push(ans.accident_date.year);
+        ans.accident_tags.push(ans.accident_location);
+        ans.accident_tags.push(ans.accident_aircraft);
+        ans.accident_tags.push(ans.accident_airline);
+        ans.accident_tags = ans.accident_tags.concat(ans.accident_icao_categories);
+
+        ans.references = [];
+
+        let pns = document.getElementsByClassName('pageNum');
+        let refs = document.getElementsByClassName('reference');
+
+        for (let index = 0; index < pns.length; index++) {
+            ans.references.push({ page_number: pns[index].value, reference: refs[index].value });
+        }
+
+        let url = window.location.href + 'makeAccident';
+        let file = new Int8Array(await document.getElementById('report').files[0].arrayBuffer());
+
+        //console.log(file);
+
+        let res = await fetch(url, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ ans, file })
+        });
+
+        let data = await res.json();
+
+        if (data.msg == 'success') {
+            window.sessionStorage.displayMessage = true;
+            window.location.reload();
+        }
+        else {
+            var toast = document.getElementById("mtoast");
+            toast.innerHTML = "FAILED TO UPLOAD";
+            toast.classList.add("show");
+            toast.style.background = '#ef522e';
+            toast.style.display = 'inline';
+
+            setTimeout(function makeMessageDisappear() {
+                toast.style.display = 'none';
+            }, 5000);
+        }
     }
+    catch (err) {
+        console.log(err);
+        var toast = document.getElementById("mtoast");
+        toast.innerHTML = "FAILED TO UPLOAD";
+        toast.classList.add("show");
+        toast.style.background = '#ef522e';
+        toast.style.display = 'inline';
 
-    ans.accident_location = document.getElementById('location').value;
-    ans.accident_aircraft = document.getElementById('aircraft').value;
-    ans.accident_airline = document.getElementById('airline').value;
-    ans.accident_fatalities = document.getElementById('fatalities').value;
-    ans.accident_synopsis = document.getElementById('synopsis').value;
-    ans.accident_avsn_link = document.getElementById('avsn_link').value;
-
-    ans.accident_icao_categories = document.getElementById('icao').value.split(",").map(function (item) {
-        return item.trim();
-    });
-
-    ans.accident_tags = document.getElementById('tags').value.split(",").map(function (item) {
-        return item.trim();
-    });
-
-    ans.accident_tags.push(months[parseInt(ans.accident_date.month) - 1]);
-    ans.accident_tags.push(ans.accident_date.year);
-    ans.accident_tags.push(ans.accident_location);
-    ans.accident_tags.push(ans.accident_aircraft);
-    ans.accident_tags.push(ans.accident_airline);
-    ans.accident_tags = ans.accident_tags.concat(ans.accident_icao_categories);
-
-    ans.references = [];
-
-    let pns = document.getElementsByClassName('pageNum');
-    let refs = document.getElementsByClassName('reference');
-
-    for (let index = 0; index < pns.length; index++) {
-        ans.references.push({ page_number: pns[index].value, reference: refs[index].value });
+        setTimeout(function makeMessageDisappear() {
+            toast.style.display = 'none';
+        }, 5000);
     }
-
-    let url = window.location.href + 'makeAccident';
-    let file = new Int8Array(await document.getElementById('report').files[0].arrayBuffer());
-
-    console.log(file);
-
-    let res = await fetch(url, {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ ans, file })
-    });
 }
